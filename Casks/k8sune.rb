cad9ad09ee045d01be88b1994b1b51169375e105
k8sune release automation <k8sune@nannan.in>
2026-08-17 03:56:54 +0530

    fix: point k8sune cask at published test5 asset

--- Casks/k8sune.rb
+++ Casks/k8sune.rb
@@ -2,7 +2,7 @@ cask "k8sune" do
   version "0.0.2b-test5"
   sha256 "ab09a59a174695327a0ce5d53c18514092d278b50b5fbdf6fe6e263b68234575"
 
-  url "https://github.com/nanna7077/k8sune/releases/download/v0.0.2b-test5/k8sune_0.0.2b-test5_universal.dmg"
+  url "https://github.com/nanna7077/k8sune/releases/download/v0.0.2b-test5/k8sune_0.1.0_universal.dmg"
   name "k8sune"
   desc "Modern and integrated desktop controller for Kubernetes"
   homepage "https://github.com/nanna7077/k8sune"
